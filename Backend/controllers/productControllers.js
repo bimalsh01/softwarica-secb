135885466ae9fa6b8d488e6c9e46b07dbe611df7
Bimal Shrestha <69587963+bimalsh01@users.noreply.github.com>
2023-12-13 08:27:28 +0545

    upto create

--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -1,6 +1,62 @@
+const cloudinary = require("cloudinary");
+const Products = require("../model/productModel")
 
-const createProduct = (req,res) => {
-    res.send("createProduct API")
+const createProduct = async (req,res) => {
+    // step 1 : check incomming data
+    console.log(req.body);
+    console.log(req.files);
+
+    // step 2 : Destructuring data
+    const {
+        productName, 
+        productPrice,
+        productDescription,
+        productCategory,
+    } = req.body;
+    const {productImage} = req.files;
+
+    // step 3 : Validate data
+    if(!productName || !productPrice || !productDescription || !productCategory || !productImage){
+        return res.json({
+            success : false,
+            message : "Please fill all the fields"
+        })
+    }
+
+    try {
+        // upload image to cloudinary
+        const uploadedImage = await cloudinary.v2.uploader.upload(
+            productImage.path,
+            {
+                folder : "products",
+                crop : "scale"
+            }
+        )
+
+        // Save to database
+        const newProduct = new Products({
+            productName : productName,
+            productPrice : productPrice,
+            productDescription : productDescription,
+            productCategory : productCategory,
+            productImageUrl : uploadedImage.secure_url
+        })
+        await newProduct.save();
+        res.json({
+            success : true,
+            message : "Product created successfully",
+            product : newProduct
+        })
+
+
+        
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : "Internal server error"
+        })
+    }
+
 }
 
 module.exports = {
